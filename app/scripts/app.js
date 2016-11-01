ad8fc6768e72f03f184d9c59ddd651c6d2cdc22a
Arthur Philadelpho <arthur22rj@gmail.com>
2016-11-01 16:34:51 -0200

    Checkpoint 4 - Templates - Complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,29 +1,29 @@
 (function(){
 
-	function config($stateProvider, $locationProvider){
+	function configure($stateProvider, $locationProvider){
 		$locationProvider
 			.html5Mode({
 				enabled: true,
 				requireBase: false
 			});
 
-			 $stateProvider
-         .state('landing', {
-             url: '/',
-             templateUrl: '/templates/landing.html'
-         })
-         .state('album', {
-             url: '/album',
-             templateUrl: '/templates/album.html'
-         })
-         .state('collection', {
-             url: '/collection',
-             templateUrl: '/templates/collection.html'
-         });
+		$stateProvider
+            .state('landing', {
+                url: '/',
+                templateUrl: '../templates/landing.html'
+            })
+            .state('album', {
+                url: '/album',
+                templateUrl: '../templates/album.html'
+            })
+            .state('collection', {
+                url: '/collection',
+                templateUrl: '../templates/collection.html'
+            });
 	}
 
 	angular
 		.module('blocJams', ['ui.router'])
-		.config(config);
+		.config(configure);
 
 })();
