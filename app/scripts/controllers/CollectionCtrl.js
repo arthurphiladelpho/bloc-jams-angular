57b5d4f0dc50113d59f3e390a3cb5cd5500f908a
Arthur Philadelpho <arthur22rj@gmail.com>
2016-11-03 19:18:41 -0200

    Collection is now working smoothly

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -1,8 +1,8 @@
  (function() {
      function CollectionCtrl() {
-     	this.album = [];
+     	this.albums = [];
      	for (var i=0; i < 12; i++){
-     		this.album.push(angular.copy(albumPicasso));
+     		this.albums.push(angular.copy(albumPicasso));
      	}
      }
  
