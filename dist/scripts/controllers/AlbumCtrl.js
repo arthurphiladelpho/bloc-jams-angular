da1b1f6f8ac4f848bd89825479ec10d7e0aed48c
Arthur Philadelpho <arthur22rj@gmail.com>
2016-11-03 21:51:14 -0200

    Checkpoint 6 - Services Pt. 1 Complete

--- dist/scripts/controllers/AlbumCtrl.js
+++ dist/scripts/controllers/AlbumCtrl.js
@@ -1,22 +1,12 @@
  (function() {
-    function AlbumCtrl() {
-     	this.albumData = {
-     		title: 'The Colors',
-     		artist: 'Pablo Picasso',
-     		label: 'Cubism',
-     		year: '1881',
-     		albumArtUrl: 'assets/images/album_covers/01.png',
-		     songs: [
-			     { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
-			     { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
-			     { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red' },
-			     { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink' },
-			     { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta' }
-		     ]
-		  }
+    //we add the Fixtures function as an argument to the Album controller
+    function AlbumCtrl(Fixtures) {
+     	this.albumData = Fixtures.getAlbum();
+
     }
  
      angular
          .module('blocJams')
-         .controller('AlbumCtrl', AlbumCtrl);
+         .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+         //we add the fixtures factory as a dependency in the controller 
  })();
