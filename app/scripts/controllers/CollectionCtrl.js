64dfe1d27948034e89d4cab01d90c4d45fc6f112
Arthur Philadelpho <arthur22rj@gmail.com>
2016-11-04 16:18:33 -0200

    Debugging App: Assignment 5 had error fixing it to get back to Checkpoint 6

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -1,9 +1,12 @@
  (function() {
-     function CollectionCtrl(Fixtures) {
-     	this.albums = Fixtures.getCollection(12);
+     function CollectionCtrl() {
+     	this.albums = [];
+     	for (var i=0; i < 12; i++){
+     		this.albums.push(angular.copy(albumPicasso));
+     	}
      }
  
      angular
          .module('blocJams')
-         .controller('CollectionCtrl', ['Fixtures', CollectionCtrl]);
+         .controller('CollectionCtrl', CollectionCtrl);
  })();
