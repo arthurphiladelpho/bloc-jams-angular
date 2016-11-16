eda15dcca196efd5687cdcd1feb0728a5d5d55e7
Arthur Philadelpho <arthur22rj@gmail.com>
2016-11-16 21:31:24 -0200

    Checkpoint 9 - Assignment Complete

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -1,5 +1,5 @@
 (function() {
-	function seekBar() {
+	function seekBar($document) {
 		//Takes in the seekBar element (volumer or song position seekBar) and the event (click or mouseDown) that occurred.
 		var calculatePercent = function(seekBar, event) {
 			// Stores the difference between the position of the click and the far left of the seekbar. 
@@ -7,12 +7,8 @@
 			// .offset() is jQuery. Allows to retrueve the position of the element relative to the document. 
 			//		- Returns an object containing the top and left properties.
 			var offsetX = event.pageX - seekBar.offset().left;
-			console.log(event.pageX);
-			console.log(seekBar.offset().left);
-			console.log(offsetX);
 			// .width() is a jQuery method that returns the width of the element it is applied to.
 			var seekBarWidth = seekBar.width();
-			console.log(seekBarWidth);
 			// Dividir (a distancia do evento da ponta esquerda da seekBar) pelo (tamanho da seekBar) ie: 70/100
 			var offsetXPercent = offsetX / seekBarWidth;
 			// Make sure this percentage is no lower than zero.
@@ -28,33 +24,59 @@
          restrict: 'E',
          scope: { },
          link: function(scope, element, attributes) {
-             scope.value = 0;
-             scope.max = 100;
- 						 // Define seekBar variable as the seekBar element we are referring to this moment. ie: volume seekBar or song position seekBar.
-             var seekBar = $(element);
+            scope.value = 0;
+            scope.max = 100;
+ 						// Define seekBar variable as the seekBar element we are referring to this moment. ie: volume seekBar or song position seekBar.
+            var seekBar = $(element);
 
-             var percentString = function () {
-                 var value = scope.value;
-                 var max = scope.max;
-                 var percent = value / max * 100;
-                 return percent + "%";
-             };
+            var percentString = function () {
+							var value = scope.value;
+							var max = scope.max;
+							var percent = value / max * 100;
+							return percent + "%";
+            };
  
-             scope.fillStyle = function() {
-                 return {width: percentString()};
-             };
+            scope.fillStyle = function() {
+							return {width: percentString()};
+            };
+            
+            scope.thumbStyle = function() {
+							return {left: percentString()};
+            };
+
              // Multiplies the percentage of the calculatePercentage by 100 and updated the scope.value variable.
              // This method may now be added as an ng-click directive to a seekBar element.
              // ie: <div class="seek-bar" ng-click="onClickSeekBar($event)">
-             scope.onClickSeekBar = function(event) {
-            	 var percent = calculatePercent(seekBar, event);
-            	 scope.value = percent * scope.max;
-         		 };
+            scope.onClickSeekBar = function(event) {
+							var percent = calculatePercent(seekBar, event);
+							scope.value = percent * scope.max;
+         		};
+
+
+         		scope.trackThumb = function() {
+     					//$document is the jQuery equivalent to window.document
+     						//.bind() is a jQuery method to attach some functionality to an event.
+     							//mousemove is an event that is fired anytime a mouse moves.
+     								//.thumb is a jQuery event namespace, used to indicate a specific event handler.
+     					$document.bind('mousemove.thumb', function(event) {
+	         			var percent = calculatePercent(seekBar, event);
+	         			//.$apply here is what makes sure the seekbar is updated every move the mouse makes.
+	         			scope.$apply(function() {
+	             		scope.value = percent * scope.max;
+	         			});
+     					});
+ 							//Here we add functionality for when the user lets go of the mouse, we stop modyfing the scope.value.
+				      $document.bind('mouseup.thumb', function() {
+         				$document.unbind('mousemove.thumb');
+         				$document.unbind('mouseup.thumb');
+     					});
+ 						};
+
    			  }
    	};
 	}
 
 	angular
 	  .module('blocJams')
-	  .directive('seekBar', seekBar);
+	  .directive('seekBar', ['$document', seekBar]);
  })();
--- dist/scripts/directives/seekBar.js
+++ dist/scripts/directives/seekBar.js
@@ -1,121 +1,80 @@
 (function() {
 	function seekBar($document) {
+		//Takes in the seekBar element (volumer or song position seekBar) and the event (click or mouseDown) that occurred.
+		var calculatePercent = function(seekBar, event) {
+			// Stores the difference between the position of the click and the far left of the seekbar. 
+			// pageX is native JavaScript. Returns the horizontal coordinate of an event relative to the whole document.
+			// .offset() is jQuery. Allows to retrueve the position of the element relative to the document. 
+			//		- Returns an object containing the top and left properties.
+			var offsetX = event.pageX - seekBar.offset().left;
+			// .width() is a jQuery method that returns the width of the element it is applied to.
+			var seekBarWidth = seekBar.width();
+			// Dividir (a distancia do evento da ponta esquerda da seekBar) pelo (tamanho da seekBar) ie: 70/100
+			var offsetXPercent = offsetX / seekBarWidth;
+			// Make sure this percentage is no lower than zero.
+			offsetXPercent = Math.max(0, offsetXPercent);
+			// Make sure this percentage is no higher than 1.
+			offsetXPercent = Math.min(1, offsetXPercent);
+			return offsetXPercent;
+ 		};
 
-	 	/**
-		* @function link
-		* @desc Calculates the horizontal percent along the seek bar where the event passed occurred.
-		* @param {HTML Element w/ the seek-bar tag} seekBar
-		*/
-	 	var calculatePercent = function(seekBar, event) {
-	 		var offsetX = event.pageX - seekBar.offset().left;
-	 		var seekBarWidth = seekBar.width();
-	 		var offsetXPercent = offsetX / seekBarWidth;
-	 		offsetXPercent = Math.max(0, offsetXPercent);
-	 		offsetXPercent = Math.min(1, offsetXPercent);
-	 		return offsetXPercent;
-		};
+		return {
+         templateUrl: '/templates/directives/seek_bar.html',
+         replace: true,
+         restrict: 'E',
+         scope: { },
+         link: function(scope, element, attributes) {
+            scope.value = 0;
+            scope.max = 100;
+ 						// Define seekBar variable as the seekBar element we are referring to this moment. ie: volume seekBar or song position seekBar.
+            var seekBar = $(element);
 
-	 	return {
-	    templateUrl: '/templates/directives/seek_bar.html',
-	    replace: true,
-	    restrict: 'E',
-	    /**
-	 		* @desc Specifies a new scope for this directive.
-			* @type {Object}
-	 		*/
-	    scope: {
+            var percentString = function () {
+							var value = scope.value;
+							var max = scope.max;
+							var percent = value / max * 100;
+							return percent + "%";
+            };
+ 
+            scope.fillStyle = function() {
+							return {width: percentString()};
+            };
+            
+            scope.thumbStyle = function() {
+							return {left: percentString()};
+            };
 
-	    	onChange: '&'
-	    	
-	    },
-	    /**
-			* @function link
-			* @desc Registers DOM listeners and updates DOM.
-			*/
-	    link: function(scope, element, attributes){
-	    				/**
-					 		* @desc Holds the value of a seek bar. ie: current volume or song time.
-							* @type {Number}
-					 		*/
-			     		scope.value = 0;
-			     		/**
-					 		* @desc Holds the maximum value of a seek bar.
-							* @type {Number}
-					 		*/
-			     		scope.max = 100;
+             // Multiplies the percentage of the calculatePercentage by 100 and updated the scope.value variable.
+             // This method may now be added as an ng-click directive to a seekBar element.
+             // ie: <div class="seek-bar" ng-click="onClickSeekBar($event)">
+            scope.onClickSeekBar = function(event) {
+							var percent = calculatePercent(seekBar, event);
+							scope.value = percent * scope.max;
+         		};
 
-			     		var seekBar = $(element);
 
-			     		attributes.$observe('value', function(newValue) {
-						     scope.value = newValue;
-						 	});
-						 
-						 	attributes.$observe('max', function(newValue) {
-						     scope.max = newValue;
-						 	});
-						 	/**
-							* @function percentString
-							* @desc Divides the scope's value property by the max property and returns it a string.
-							*/
-			     		var percentString = function(){
-			     			var value = scope.value;
-			     			var max = scope.max;
-			     			var percent = value / max * 100;
-			     			return percent + "%";
-			     		};
-			     		/**
-							* @function fillStyle
-							* @desc Returns the width of the seek bar fill element based on the percentage calculated.
-							*/
-			     		scope.fillStyle = function() {
-			     			return {width: percentString()};
-			     		};
+         		scope.trackThumb = function() {
+     					//$document is the jQuery equivalent to window.document
+     						//.bind() is a jQuery method to attach some functionality to an event.
+     							//mousemove is an event that is fired anytime a mouse moves.
+     								//.thumb is a jQuery event namespace, used to indicate a specific event handler.
+     					$document.bind('mousemove.thumb', function(event) {
+	         			var percent = calculatePercent(seekBar, event);
+	         			//.$apply here is what makes sure the seekbar is updated every move the mouse makes.
+	         			scope.$apply(function() {
+	             		scope.value = percent * scope.max;
+	         			});
+     					});
+ 							//Here we add functionality for when the user lets go of the mouse, we stop modyfing the scope.value.
+				      $document.bind('mouseup.thumb', function() {
+         				$document.unbind('mousemove.thumb');
+         				$document.unbind('mouseup.thumb');
+     					});
+ 						};
 
-			     		scope.thumbStyle = function(){
-			     			return {width: trackThumb()};
-			     		}
-			     		/**
-							* @function onClickSeekBar
-							* @desc Updates the seek bar value based on seek bar width and location user clicked.
-							*/
-			     		scope.onClickSeekBar = function(event) {
-			         	var percent = calculatePercent(seekBar, event);
-			         	scope.value = percent * scope.max;
-			         	notifyOnChange(scope.value);
-			     		};
-			     		/**
-							* @function trackThumb
-							* @desc Similar to onClickSeekBar but uses $apply to constantly apply the change in value of scope.value as user drags the seek bar thumb.
-							*/
-			     		scope.trackThumb = function() {
-			 					$document.bind('mousemove.thumb', function(event) {
-			    				var percent = calculatePercent(seekBar, event);
-			     				scope.$apply(function() {
-			         			scope.value = percent * scope.max;
-			         			notifyOnChange(scope.value);
-			     				});
-			 					});
-
-			 					
-			 				/**
-							* @function notifyOnChange
-							* @desc Sets the value variable to newValue in a new passed in function.
-							* @param newValue depends on the function. For our purposes a number.
-							*/
-			 				var notifyOnChange = function(newValue) {
-								if (typeof scope.onChange === 'function') {
-							     scope.onChange({value: newValue});
-								}
-							};
-
-			 					$document.bind('mouseup.thumb', function() {
-			     				$document.unbind('mousemove.thumb');
-					        $document.unbind('mouseup.thumb');
-			 					});
-							};
-	    	}
-	 		};
-		}
+   			  }
+   	};
+	}
 
 	angular
 	  .module('blocJams')
