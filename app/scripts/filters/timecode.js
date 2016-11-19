96f1fbdfa24cd74eb5de9cab886482c37a0abd21
Arthur Philadelpho <arthur22rj@gmail.com>
2016-11-19 15:19:57 -0200

    Added Filter to Album View : Checkpoint 11 Complete

--- app/scripts/filters/timecode.js
+++ app/scripts/filters/timecode.js
@@ -1,27 +1,27 @@
 (function() {
      function timecode() {
-         return function(seconds) {
+       return function(seconds) {
 
-       		var seconds = Number.parseFloat(seconds);
+     		var seconds = Number.parseFloat(seconds);
 
-       		if (Number.isNaN(seconds)) {
-         		return '-:--';
-     			}
+     		if (Number.isNaN(seconds)) {
+       		return '-:--';
+   			}
 
-          var wholeSeconds = Math.floor(seconds);
-          var minutes = Math.floor(wholeSeconds / 60);
-          var remainingSeconds = wholeSeconds % 60;
+        var wholeSeconds = Math.floor(seconds);
+        var minutes = Math.floor(wholeSeconds / 60);
+        var remainingSeconds = wholeSeconds % 60;
 
-          var output = minutes + ':';
+        var output = minutes + ':';
 
-          if (remainingSeconds < 10) {
-               output += '0';   
-          }
+        if (remainingSeconds < 10) {
+             output += '0';   
+        }
 
-          output += remainingSeconds;
+        output += remainingSeconds;
 
-          return output;
-         };
+        return output;
+       };
      }
  
      angular
